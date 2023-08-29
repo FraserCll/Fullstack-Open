7f27493014fb0ef8f60bbb6daa664510c79b3280
FraserCll <fcfrasca@gmail.com>
2023-08-29 12:19:15 +0100

    Completed 2.9 force lowercaps filter input :)

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-1234567', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterName, setFilterName] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -23,7 +27,6 @@ const App = () => {
       setNewName('')
       setNewNumber('')
     }
-
   }
 
   const handleNameChange = (event) => {
@@ -34,9 +37,22 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilter = (event) => {
+    setFilterName(event.target.value.toLowerCase())
+  }
+
+  const personsToShow = filterName
+    ? persons.filter(person => person.name.toLowerCase().includes(filterName))
+    : persons
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        <input type='text' value={filterName}
+        onChange={handleFilter} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input type='text' value={newName}
@@ -52,7 +68,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person, index) => (
+        {personsToShow.map((person, index) => (
           <li key={index}>{person.name} {person.number}</li>
         ))}
       </ul>
