3c67dea41e990dcc2675f358a6580d847c67f367
FraserCll <fcfrasca@gmail.com>
2023-09-13 14:56:26 +0100

    Search & render conditions

--- Part2/countries/src/App.js
+++ Part2/countries/src/App.js
@@ -1,13 +1,54 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const FindCountries = ({ handleFindCountries }) => {
+  return (
+    <div>
+    find countries <input type='text'
+    onChange={handleFindCountries} />
+    </div>
+  )
+}
+
+// Display countries with name containing term 'search'
+const Countries = ({ countries, search }) => {
+  const countriesToShow = search
+  ? countries.filter(country => country.toLowerCase().includes(search))
+  : countries
+
+  console.log(countriesToShow.length)
+
+  if ((10 < countriesToShow.length) && (search.length != 0 )) {
+    return (
+      <p>
+      Too many matches, specify another filter
+      </p>
+    )
+  }
+  else if ( ( countriesToShow.length > 1 ) && (countriesToShow.length < 11) ) {
+    return (
+      <ul>
+        {countriesToShow.map((country, i) => 
+          <li key={i}>
+            {country}
+          </li>
+        )}
+      </ul>
+    )
+  }
+  else if (countriesToShow.length === 1) {
+    console.log(`Display single country data ${countriesToShow.toString().toLowerCase()}` )
+  }
+}
+
 const App = () => {
   const [countries, SetCountries] = useState([])
-  const [findCountries, setFindCountries] = useState('')
-  const fetchedCountries = []
+  const [countryData, setCountryData] = useState()
+  const [countryToFetch, setCountryToFetch] = useState(null)
+  const [search, setSearch] = useState('')
 
+  // Get the list of country (common) names
   useEffect(() => {
-    console.log('fetching countries...')
       axios
         .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
         .then(response => {
@@ -17,7 +58,28 @@ const App = () => {
         .catch( error => {
           console.log('error fetching countries')
         })
-      }, [])
+      }, []
+  )
+
+  // Get data for specific country
+  useEffect(() => {
+    if (countryToFetch) {
+      console.log('fetching country data...')
+      axios
+        .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${countryToFetch}`)
+        .then(response => {
+            setCountryData = response.data
+        })
+        .catch( error => {
+          console.log('error fetching country')
+        })
+    }
+  }, []
+  )
+
+  const handleFindCountries = (event) => {
+    setSearch(event.target.value.toLowerCase())
+  }
 
   return (
     <div>
@@ -25,18 +87,13 @@ const App = () => {
         Countries
       </h1>
       <p>
-        First we will work on a solution which fetches all the common names of the countries and diplays them in a nice list below.
+        Now we will create the search component 'find countries', and display matches.
       </p>
       <h2>
-        List of all countries
+        Search
       </h2>
-      <ul>
-        {countries.map((country, i) =>
-          <li key={i}>
-            {country}
-          </li>
-        )}
-      </ul>
+      <FindCountries handleFindCountries={handleFindCountries} />
+      <Countries countries={countries} search={search} />
     </div>
   )
 }
