1b9ab2b597577b16bf15252bba9244ed1dd1cd7f
FraserCll <fcfrasca@gmail.com>
2023-10-19 16:06:38 +0100

    Database configuration into its own module

--- Part3/phonebook/index.js
+++ Part3/phonebook/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(cors())
 app.use(express.json())
@@ -18,6 +20,35 @@ app.use(morgan(function (tokens, req, res) {
     ].join(' ')
   }))
 
+
+/*
+// below section I go back to follow 'connecting the backend to a database'
+// checked and working.
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+const url = `mongodb+srv://fullstack:${password}@cluster0.0nnphtl.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: Number,
+})
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+const Person = mongoose.model('Person', personSchema)
+*/
+
+/*
 let persons = [
     { 
       "id": 1,
@@ -40,13 +71,16 @@ let persons = [
       "number": "39-23-6423122"
     }
 ]
+*/
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -66,6 +100,7 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
 }
 
+/* before making backend functionality to use the database
 app.post('/api/persons', (request, response) => {
     const person = request.body
     const name = person.name
@@ -85,8 +120,25 @@ app.post('/api/persons', (request, response) => {
         persons = persons.concat(person)
         response.json(person)
     }
-    
 })  
+*/
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (body.name === undefined) {
+        return response.status(400),json({ error: 'content missing' })
+    }
+
+    const person = new Person({
+        name: body.name,
+        number: body.important
+    })
+
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
+})
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
@@ -95,9 +147,17 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+/* old - before backend integrate to DB
 app.get('/info', (request, response) => {
     const datetime = new Date()
     response.send(`<p>Phonebook has info for ${persons.length} people<br/>${datetime}</p>`)
+})
+*/
+
+app.get('/api/persons/:id', (request, response) => {
+    Person.findById(request.params.id).then(person => {
+        response.json(person)
+    })
 })
 
 const format = (tokens, req, res) => {
@@ -105,7 +165,7 @@ const format = (tokens, req, res) => {
     next()
 }
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
