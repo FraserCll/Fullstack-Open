3718c1c89600c519bc89917363fe299b91cca535
FraserCll <fcfrasca@gmail.com>
2023-10-26 16:25:55 +0100

    Completed exercises 3.15 & 3.16

--- Part3/phonebook/index.js
+++ Part3/phonebook/index.js
@@ -64,18 +64,38 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
-})
-
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+        response.status(204).end()
     })
+    .catch(error => next(error))
 })
+
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+    .then(person => {
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError' ) {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+//errorHandler needs to be the last loaded middleware
+app.use(errorHandler)
 
 const format = (tokens, req, res) => {
 
