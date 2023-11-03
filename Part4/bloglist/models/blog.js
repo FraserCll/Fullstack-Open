00305e57cad8c66ba59a5be9508d38356290c7b5
FraserCll <fcfrasca@gmail.com>
2023-11-03 16:17:24 +0000

    Completed exercise 4.2

--- Part4/bloglist/models/blog.js
+++ Part4/bloglist/models/blog.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-const config = require('../utils/config')
 
 const blogSchema = new mongoose.Schema({
     title: String,
@@ -8,17 +7,12 @@ const blogSchema = new mongoose.Schema({
     likes: Number
   })
   
-  blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-  })
-  
-  const Blog = mongoose.model('Blog', blogSchema)
-  
-  const mongoUrl = config.MONGODB_URI
-  mongoose.connect(mongoUrl)
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+  }
+})
 
-  module.exports = mongoose.model('Blog', blogSchema)
+module.exports = mongoose.model('Blog', blogSchema)
