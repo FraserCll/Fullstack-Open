7c5a8a78d55dc0827007b8beef0efb4e68a03536
FraserCll <fcfrasca@gmail.com>
2023-11-09 16:33:28 +0000

    Completed exercise 4.5*

--- Part4/bloglist/utils/list_helper.js
+++ Part4/bloglist/utils/list_helper.js
@@ -8,6 +8,12 @@ const totalLikes = (blogs) => {
     : blogs.reduce((sum, blogs) => sum + blogs.likes, 0)
 }
 
+const favouriteBlog = (blogs) => {
+    return blogs.length === 0
+    ? 0
+    : blogs.reduce((favourite, blog) => blog.likes > favourite.likes ? blog : favourite, blogs[0] )
+}
+
 module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favouriteBlog
 }
